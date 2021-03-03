98add686fd5227de7a4ba912d4b5fe1adf0a1133
Anairkoen Schno <aaron.cirr.com@gmail.com>
2021-03-03 02:58:18 -0600

    Add thunks for this ptr no return buffer and this ptr with return buffer

--- RuntimeDetour/Platforms/Generic/GenericDetourCoreCLRWinX64.cs
+++ RuntimeDetour/Platforms/Generic/GenericDetourCoreCLRWinX64.cs
@@ -15,6 +15,7 @@ namespace MonoMod.Common.RuntimeDetour.Platforms.Generic {
         // immediately following that index will be a 16 bit length of the call instruction (to determine how to backpatch)
 
         // we use ;# for comments in assembly to be slightly more portable across assemblers
+
         /**** this pointer context thunk assembly ****\
         pop r10 ;# r10 isn't used to pass arguments on Windows; it now contains the return position
 
@@ -57,6 +58,127 @@ namespace MonoMod.Common.RuntimeDetour.Platforms.Generic {
             0x52,                           // push rdx
             0x41, 0x50,                     // push r8
             0x41, 0x51,                     // push r9
+            0x4D, 0x89, 0xD0,               // mov r8, r10
+            0x48, 0x31, 0xD2,               // xor rdx, rdx
+            0x66, 0x41, 0x8B, 0x52, 0x0C,   // mov dx, [r10 + 12]
+            0x49, 0x29, 0xD0,               // sub r8, rdx
+            0x48, 0x31, 0xD2,               // xor rdx, rdx
+            0x41, 0x8B, 0x52, 0x08,         // mov edx, [r10 + 8]
+            0x41, 0xFF, 0x12,               // call [r10]
+            0x41, 0x59,                     // pop r9
+            0x41, 0x58,                     // pop r8
+            0x5A,                           // pop rdx
+            0x59,                           // pop rcx
+            0xFF, 0xE0                      // jmp rax
+        };
+
+        /**** instance no return buffer generic cookie/static return buffer generic cookie ****
+        ;#   the position of the generic cookie is the same when there is a this pointer and no return buffer as if there is 
+        ;# no this pointer and a return buffer
+
+        pop r10 ;# get the return address for where we're calling from
+        
+        ;# save register-passed arguments
+        push rcx
+        push rdx
+        push r8
+        push r9
+
+        ;# setup call
+        ;# the methods being called here have no strangeness, only user args passed in register
+
+        ;# first arg is the generic cookie, which is in rdx
+        mov rcx, rdx
+
+        ;# the third arg is the source start
+        mov r8, r10 ;# r8 is the argument we want it in
+        xor rdx, rdx
+        mov dx, [r10 + 12] ;# offset of the call insn length
+        sub r8, rdx
+
+        ;# the second arg is the index
+        xor rdx, rdx
+        mov edx, [r10 + 8] ;# offset of the index
+
+        ;# finally call handler
+        call [r10]
+
+        ;# rax now contains our target method, but we need to re-load our arguments
+        pop r9
+        pop r8
+        pop rdx
+        pop rcx
+
+        ;# we're finally ready to call our target
+        jmp rax
+        */
+        private static readonly byte[] ThisPtrNoBufThunk = {
+            0x41, 0x5A,                     // pop r10
+            0x51,                           // push rcx
+            0x52,                           // push rdx
+            0x41, 0x50,                     // push r8
+            0x41, 0x51,                     // push r9
+            0x48, 0x89, 0xD1,               // mov rcx, rdx
+            0x4D, 0x89, 0xD0,               // mov r8, r10
+            0x48, 0x31, 0xD2,               // xor rdx, rdx
+            0x66, 0x41, 0x8B, 0x52, 0x0C,   // mov dx, [r10 + 12]
+            0x49, 0x29, 0xD0,               // sub r8, rdx
+            0x48, 0x31, 0xD2,               // xor rdx, rdx
+            0x41, 0x8B, 0x52, 0x08,         // mov edx, [r10 + 8]
+            0x41, 0xFF, 0x12,               // call [r10]
+            0x41, 0x59,                     // pop r9
+            0x41, 0x58,                     // pop r8
+            0x5A,                           // pop rdx
+            0x59,                           // pop rcx
+            0xFF, 0xE0                      // jmp rax
+        };
+        // ^^^ the above is also used when there is no this pointer but there is a return buffer
+
+
+        /**** instance return buffer generic cookiee ****
+        pop r10 ;# get the return address for where we're calling from
+        
+        ;# save register-passed arguments
+        push rcx
+        push rdx
+        push r8
+        push r9
+
+        ;# setup call
+        ;# the methods being called here have no strangeness, only user args passed in register
+
+        ;# first arg is the generic cookie, which is in rdx
+        mov rcx, r8
+
+        ;# the third arg is the source start
+        mov r8, r10 ;# r8 is the argument we want it in
+        xor rdx, rdx
+        mov dx, [r10 + 12] ;# offset of the call insn length
+        sub r8, rdx
+
+        ;# the second arg is the index
+        xor rdx, rdx
+        mov edx, [r10 + 8] ;# offset of the index
+
+        ;# finally call handler
+        call [r10]
+
+        ;# rax now contains our target method, but we need to re-load our arguments
+        pop r9
+        pop r8
+        pop rdx
+        pop rcx
+
+        ;# we're finally ready to call our target
+        jmp rax
+        */
+        private static readonly byte[] ThisPtrBufThunk = {
+            0x41, 0x5A,                     // pop r10
+            0x51,                           // push rcx
+            0x52,                           // push rdx
+            0x41, 0x50,                     // push r8
+            0x41, 0x51,                     // push r9
+            0x4C, 0x89, 0xC1,               // mov rcx, r8
             0x4D, 0x89, 0xD0,               // mov r8, r10
             0x48, 0x31, 0xD2,               // xor rdx, rdx
             0x66, 0x41, 0x8B, 0x52, 0x0C,   // mov dx, [r10 + 12]
