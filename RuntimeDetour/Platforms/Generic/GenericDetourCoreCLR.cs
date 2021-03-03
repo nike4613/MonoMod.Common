19560e45d8ac2789820a8e17b3fae0b25d4c24e6
Anairkoen Schno <aaron.cirr.com@gmail.com>
2021-03-03 00:45:24 -0600

    Describe the plan for how generic detours are going to work (on CoreCLR-based runtimes (.NET Core, .NET Framework))

--- RuntimeDetour/Platforms/Generic/GenericDetourCoreCLR.cs
+++ RuntimeDetour/Platforms/Generic/GenericDetourCoreCLR.cs
@@ -4,7 +4,49 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 
-namespace MonoMod.Common.RuntimeDetour.Platforms.Generic {
+namespace MonoMod.Common.RuntimeDetour.Platforms {
+
+    // There are 4 cases we need to handle:
+    // - Unshared generics. This is trivial.
+    // - Shared, using MethodDesc as a generic cookie
+    // - Shared, using MethodTable as a generic cookie
+    // - Shared, using this as the generic cookie
+
+    //   In the first case, we don't need to do anything fancy, and can instead, at hooked method JIT time, 
+    // generate the appropriate detour target instantiation, just like a normal detour.
+
+    //   In the second case, the method itself has generic parameters and is static, and so we need to do the generic
+    // parameter thunk strategy.
+
+    //   In the third case, the method itself does not have generic parameters, but its containing type does,
+    // and it is static, so we again need to do the thunk strategy.
+
+    //   In the fourth case, the method is an instance method and does not have generic parameters itself,
+    // and uses the target object as a MethodTable to determine which types it has, and so we need to do the
+    // generic thunk strategy.
+
+    //   The generic thunk strategy involves having a thunk that we detour a method to instead of its target,
+    // because we don't know what target implementation to use at detour time. This thunk will do several things:
+    //   1. Determine the value that is used for the generic context
+    //   2. Call a C# (static) method that can handle that specific kind of context, which determines an actual
+    //      target implementation and returns a pointer to its body, as well as fixing up the call to the thunk
+    //      to point to the actual body
+    //   3. Jump to the actual body
+    // Because this relies on fragments of assembly, each architecture requires its own implementation of this
+    // which does the same basic things for that architecture. For some architectures, the calling conventions
+    // also vary based on operating system, and so there must be variants for that as well.
+    //   We need to have the thunks in place to handle generic constraints. For methods which are shared, the
+    // shared arguments are filled with System.__Canon, and that will meet no constraints other than a bare class
+    // constraint. The runtime, of course, doesn't let us construct generic types unless the provided types meet
+    // the constraints, and the JIT isn't given anything other that System.__Canon for types that will be fillled
+    // like that, so we have to wait until a given instantiation is actually called to figure out where it needs
+    // to detour to. This is what the thunks do.
+    //   Importantly, there will need to be a total of 6 thunk bodies: one which is capable obtaining each context
+    // object, for each possible position of it; there can optionally be a return buffer pointer between the this
+    // argument and the generic context. This can be reduced to just 3 however; the this object (with the exception
+    // of Framework <4.5) is always the first argument, eliminating the need for one of the bodies, and the generic
+    // cookie pointer for both the MethodDesc and MethodTable cases are in the same place, removing another two.
+
 #if !MONOMOD_INTERNAL
     public
 #endif
